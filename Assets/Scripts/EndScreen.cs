ade81c5ff3391062eff074219af21caaa0bd969d
BaranMcr <baran.mucur19@gmail.com>
2023-02-22 15:40:55 +0300

    Quiz Master v1.0

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndScreen : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
+    void Awake()
+    {
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
+    }
+
+    public void ShowFinalScore()
+    {
+        finalScoreText.text = "Tebrikler\nPuanınız: " + 
+                              scoreKeeper.CalculateScore() + "%";
+    }
+}
